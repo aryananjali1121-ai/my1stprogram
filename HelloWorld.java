ebc4d254eed22fba1c1f00d6c965f37a8e334bf7
Aryan <aryananjali1121@gmail.com>
2026-06-30 21:41:45 +0530

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[]args){
+            System.out.println("HelloWorld");
+            System.out.println("I am going to change myself in 2026");
+
+    }
+}
+
+
